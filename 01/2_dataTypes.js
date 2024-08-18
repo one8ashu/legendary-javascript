d06ce04488b177223c0a87e59626aed843312807
Shreyansh Srivastava <94685597+one8ashu@users.noreply.github.com>
2024-08-18 09:54:30 +0000

    Datatypes and ECMA standards

---
+++ 01/2_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict";  // treats all JS code as newer version
+
+// alert(3 + 3);  we are using nodejs, not browser
+// ; improves code readibility
+
+// https://tc39.es/ javascript docs
+// https://tc39.es/ecma262/ javascript docs
+
+// Data types
+
+// Primitive datatypes
+let name = "Aashu" // string
+let age = 18 // integer upto 2^53, otherwise bigint 
+let isLoggedIn = true // boolean, true or false
+// null is standalone value, is representation of empty value
+// undefined 
+// symbol used for unique reprentation
+
+// object, null is an object
+
+console.log(typeof "aashu");
+console.log(typeof null);
+console.log(typeof undefined);
